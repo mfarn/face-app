88e0585440273db63b3958e0a7578562c14f3303
Mateus Felipe <mfarn@cin.ufpe.br>
2021-12-01 23:04:17 -0300

    model recognition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from get_loader import Load_Data
+from model import Face_Recognition
 
 load_data = Load_Data('/Users/mateus/Desktop/Code/Python/pythonProject')
 loaded = load_data.Load()
 
 print(loaded[1])
+
+test_model = Face_Recognition('/Users/mateus/Desktop/Code/Python/pythonProject/data.pt')
+test_model.identify_person()
--- model.py
+++ model.py
@@ -1,49 +1,47 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch as torch
-from load_data import Load_Data 
+from get_loader import Load_Data 
 from torch.utils.data import DataLoader as DataLoader
 from PIL import Image as Image
 import cv2 as cv
-import time
-import os
 
-class Find_Face:
+class Face_Recognition:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40) # apenas um rosto
+        self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
+        self.data = torch.load(root_dir)
+        self.embedding_list = self.data[0]
+        self.name_list = self.data[1]
+        self.cam = cv.VideoCapture(0)   
 
-    mtcnn = MTCNN(image_size=240, margin=0, keep_all=True, min_face_size=40) # lista de rostos
-    resnet = InceptionResnetV1(pretrained='vggface2').eval()
-
-    embedding_list = Load_Data.load_data()[0]
-    name_list = Load_Data.load_data()[1]
-
-    def identify(self):
-
-        cam = cv.VideoCapture(0)
+    def identify_person(self):
 
         while True:
-            ret, frame = cam.read()
+            ret, frame = self.cam.read()
             if not ret:
-                print("fail to grab frame, try again")
+                print("Failed to grab a frame, please try again!")
                 break
 
             img = Image.fromarray(frame)
-            img_cropped_list, prob_list = mtcnn(img, return_prob=True)
+            img_cropped_list, prob_list = self.mtcnn(img, return_prob=True)
 
             if img_cropped_list is not None:
-                boxes, _ = mtcnn.detect(img)
+                boxes, _ = self.mtcnn.detect(img)
 
                 for i, prob in enumerate(prob_list):
                     if prob>0.90:
-                        emb = resnet(img_cropped_list[i].unsqueeze(0)).detach()
+                        emb = self.resnet(img_cropped_list[i].unsqueeze(0)).detach()
 
                         dist_list = [] # lista com distancias, menor distancia determina a pessoa
 
-                        for idx, emb_db in enumerate(embedding_list):
+                        for idx, emb_db in enumerate(self.embedding_list):
                             dist = torch.dist(emb, emb_db).item()
                             dist_list.append(dist)
 
                         min_dist = min(dist_list) # pegar menor distancia
                         min_dist_idx = dist_list.index(min_dist)
-                        name = name_list[min_dist_idx]
+                        name = self.name_list[min_dist_idx]
 
                         box = boxes[i] 
                         
@@ -55,3 +53,15 @@ class Find_Face:
                         frame = cv.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
 
             cv.imshow("IMG", frame)
+
+            cv.imshow("IMG", frame)
+    
+            k = cv.waitKey(1)
+            if k%256==27: #ESC
+                print('Esc pressed, closing...')
+                break
+
+        self.cam.release()
+        cv.destroyAllWindows()
+
+        
